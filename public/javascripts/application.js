3405c30683ca72d7004b38ba1c93c355331ff9e8
Steven Garcia <stevendgarcia@gmail.com>
2011-01-17 17:13:36 +0100

    Still no worky

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,11 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function init_uploader() {
+  var form = $('new_upload');
+  $('upload').on('click', function() {
+    form.uploader = new Uploader(form);
+    form.submit();
+  });
+}
+
+$(document).on('ready', init_uploader())
